a2f4b660c77ad8734cf0663bf4852ee98917320b
Veli Hesenli <BD7RGSR0D@code.edu.az>
2024-04-23 11:28:22 +0400

    completed

---
+++ Json/CsFileStreamFileIO/CsFileStreamFileIO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+namespace CsFileStreamFileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clothes clothes = new Clothes
+            {
+                Marka = "Vasmoy",
+                Model = "Satici ozu de bilmir",
+                Say = 321,
+                Type = "Shalvar",
+                Price=15
+
+            };
+            string fileName = "Clothes.json";
+            string jsonstr=JsonSerializer.Serialize(clothes);
+            File.WriteAllText(fileName, jsonstr);
+            Console.WriteLine(File.ReadAllText(fileName));
+
+        }
+    }
+    public class Clothes
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Say { get; set; }
+        public string Type { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Xml/XML/XML/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        XmlDocument xmlDocument = new XmlDocument();
+
+        XmlElement rootNode = xmlDocument.CreateElement("Mektebler");
+        xmlDocument.AppendChild(rootNode);
+
+        XmlElement mektebNode1 = xmlDocument.CreateElement("Mekteb");
+        rootNode.AppendChild(mektebNode1);
+
+        XmlElement adNode1 = xmlDocument.CreateElement("Ad");
+        adNode1.InnerText = "ADHU";
+        mektebNode1.AppendChild(adNode1);
+
+        XmlElement dirrectorrNode1 = xmlDocument.CreateElement("Director");
+        dirrectorrNode1.InnerText = "Keramet Boyukchol";
+        mektebNode1.AppendChild(dirrectorrNode1);
+
+        Console.WriteLine(xmlDocument.InnerXml);
+    }
+}
